1aba4b749267b455ef3d529a919658044b667747
hapdev05 <demoover81@gmail.com>
2024-11-04 23:47:17 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import Letgo4 from "./components/Letgo4";
 function App() {
   return (
     <>
-      <Router basename="/Silent_English">
+      <Router basename="/Silent_English/">
         <Routes>
-          <Route path="/Silent_English" element={<HomePage/>} />
-          <Route path="/let-go-3" element={<Letgo3/>} />
-          <Route path="/let-go-4" element={<Letgo4/>} />
+          <Route path="/Silent_English/" element={<HomePage/>} />
+          <Route path="/let-go-3/" element={<Letgo3/>} />
+          <Route path="/let-go-4/" element={<Letgo4/>} />
         </Routes>
       </Router>
     </>
